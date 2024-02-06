ebefc370aaecb2cb8cb64329b37b86e5c50d0995
Ramon Felipe Alves de Arruda Silva <ramon.felipe@hotmail.com>
2024-02-06 20:31:21 +0000

    creating userController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MyGraphQl.Application;
+
+namespace MyGraphQl.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+	private readonly ILogger<UserController> _logger;
+	private readonly IUserApplication _userApplication;
+
+    public UserController(ILogger<UserController> logger, IUserApplication userApplication)
+    {
+        _logger = logger;
+        this._userApplication = userApplication;
+    }
+
+    [HttpGet(template: "/{id:int}", Name = "GetUsers")]
+	public async Task<IActionResult> Get(int id)
+	{
+        var user = await this._userApplication.GetUser(id);
+
+        return this.Ok(user);
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using MyGraphQl.Application.IoC;
+using MyGraphQl.Infrastructure.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services
+    .AddApplications()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
